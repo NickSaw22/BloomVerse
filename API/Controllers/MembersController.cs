b3a832d3e5a762e159d10f937775758fb62088e9
NikhilSawant2-gep <59115090+NickSaw22@users.noreply.github.com>
2026-02-02 20:17:19 +0530

    added inital changes api skeleton

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDBContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetMembers()
+        {
+            var users = await context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMemberById(string id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMember([FromBody] API.Entities.AppUser user)
+        {
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetMembers), new { id = user.Id }, user);
+        }
+    }
+}
---
+++ API/Data/AppDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Entities;
+namespace API.Data
+{
+    public class AppDBContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set;}
+    }
+}
