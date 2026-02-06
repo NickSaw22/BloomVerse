a323069df486ca8e0fbad0c9adcbf4c95da0f79a
NikhilSawant2-gep <59115090+NickSaw22@users.noreply.github.com>
2026-02-06 13:14:56 +0530

    added login and register to client

---
+++ Client/src/features/home/home.ts
@@ -0,0 +1,18 @@
+import { Component, Input, signal } from '@angular/core';
+import { Register } from "../register/register";
+import { required } from '@angular/forms/signals';
+import { User } from '../../types/user';
+
+@Component({
+  selector: 'app-home',
+  imports: [Register ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  protected registerMode = signal(false);
+
+  showRegisterMode(val: boolean) {
+    this.registerMode.set(val);
+  }
+}
