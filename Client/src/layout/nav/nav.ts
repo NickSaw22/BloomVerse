6146c90f05fb6ad5e8d646c820ea68b432c04b77
NikhilSawant2-gep <59115090+NickSaw22@users.noreply.github.com>
2026-02-12 20:06:08 +0530

    added themes adnd pipes

--- Client/src/layout/nav/nav.ts
+++ Client/src/layout/nav/nav.ts
@@ -1,21 +1,37 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { AccountService } from '../../core/services/account-service';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 import { ToastService } from '../../core/services/toast-service';
-
+import { themes } from '../theme';
 @Component({
   selector: 'app-nav',
   imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
-export class Nav {
+export class Nav implements OnInit {
   protected accountService = inject(AccountService);
   private toastService = inject(ToastService)
   private router = inject(Router);
   protected creds: any = {}
   protected isLoggedIn = signal(false);
+  protected selectedTheme = signal<string>(localStorage.getItem('theme') || 'light')
+  protected themes = themes;
+    
+  ngOnInit() {
+    document.documentElement.setAttribute('data-theme', this.selectedTheme());
+  }
+
+  handleSelectedTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+    const elem = document.activeElement as HTMLDivElement;
+    if(elem){
+      elem.blur();
+    }
+  }
 
   userLogin() {
     console.log('Login button clicked with credentials:', this.creds);
