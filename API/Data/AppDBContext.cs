c1f9871dd5f69f77ea98a753b9d24871770ff3a7
NikhilSawant2-gep <59115090+NickSaw22@users.noreply.github.com>
2026-02-16 21:03:27 +0530

    added pagination, filter and sorting

--- API/Data/AppDBContext.cs
+++ API/Data/AppDBContext.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using API.Entities;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
 namespace API.Data
 {
     public class AppDBContext(DbContextOptions options) : DbContext(options)
@@ -7,6 +10,25 @@ namespace API.Data
         public DbSet<AppUser> Users { get; set;}
         public DbSet<Member> Members { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+                v => v.ToUniversalTime(),
+                v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
+            );
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(DateTime))
+                    {
+                        property.SetValueConverter(dateTimeConverter);
+                    }
+                }
+            }
+        }
     }
 }
--- Client/src/core/interceptors/loading-interceptor.ts
+++ Client/src/core/interceptors/loading-interceptor.ts
@@ -1,4 +1,4 @@
-import { HttpEvent, HttpInterceptorFn } from '@angular/common/http';
+import { HttpEvent, HttpInterceptorFn, HttpParams } from '@angular/common/http';
 import { delay, finalize, of, tap } from 'rxjs';
 import { BusyService } from '../services/busy-service';
 import { inject } from '@angular/core';
@@ -8,8 +8,15 @@ const cache = new Map<string, HttpEvent<any>>();
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService);
 
+  const generateCacheKey = (url: string, params: HttpParams): string => {
+    const paramString = params.keys().map(key => `${key}=${params.get(key)}`).join('&');
+    return paramString ? `${url}?${paramString}` : url;
+  }
+
+  const cacheKey = generateCacheKey(req.url, req.params);
+
   if(req.method === 'GET') {
-    const cachedResponse = cache.get(req.url);
+    const cachedResponse = cache.get(cacheKey);
     if(cachedResponse) {
       return of(cachedResponse);
     }
@@ -20,7 +27,7 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
     delay(500),
     tap(response=> {
       if(req.method === 'GET') {
-        cache.set(req.url, response);
+        cache.set(cacheKey, response);
       }
     }),
     finalize(() => busyService.idle())
